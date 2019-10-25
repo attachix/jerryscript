126b7585b0410568b6a994ed70fd6d033baf6438
Slavey Karadzhov <slav@attachix.com>
2019-10-25 17:53:40 +0200

    The snapshot compiler should now work also with the latest Emscripten and JerryScript.

--- emscripten-snapshot-compiler/post.js
+++ emscripten-snapshot-compiler/post.js
@@ -12,92 +12,72 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var Module;
-var ENVIRONMENT_IS_NODE;
 
-(function(){
-  Module.prototype.snapshotVersion = Module.ccall(
-    'emscripten_snapshot_compiler_get_version', 'number');
+Module['onRuntimeInitialized'] = function() {
+  // When invoked as the main Node.js entry point, also expose a basic CLI:
+  if (ENVIRONMENT_IS_NODE && require.main === module) {
+    function printUsageAndExit() {
+      var path = require('path');
+      var scriptFile = path.basename(__filename);
+      console.log(
+  `Generates JerryScript snapshot file from JavaScript source file.
 
-  Module.inspect = Module.toString = function() {
-    return Module.prototype.toString();
-  };
-})();
+    ${scriptFile} [options] IN_FILE
 
-// Set module.exports or else expose constructor on global object:
-if (typeof module !== 'undefined') {
-  module.exports = Module;
-} else {
-  var global = (new Function('return this;'))();
-  global.JerryScriptSnapshotCompiler = Module;
-}
-
-// When invoked as the main Node.js entry point, also expose a basic CLI:
-if (ENVIRONMENT_IS_NODE && require.main === module) {
-  function printUsageAndExit() {
-    var path = require('path');
-    var scriptFile = path.basename(__filename);
-    console.log(
-`Generates JerryScript snaphot file from JavaScript source file.
-
-  ${scriptFile} [options] IN_FILE
-
-OPTIONS:
-  -o | --output    Snapshot output file path -- default is "snapshot.out"
-  -s | --strict    Source should be interpreted in strict mode -- default is false
-  -e | --eval      Code should be executed as eval() -- default is execute as global`);
-    process.exit(-1);
-  }
-
-  // inputPath, isForGlobal, isStrict, outputPath
-  var options = {
-    inputPath: undefined,
-    outputPath: 'snapshot.out',
-    isForGlobal: true,
-    isStrict: false,
-  };
-
-  var nextKey;
-  process.argv.slice(1).forEach((val, index) => {
-    switch (val) {
-      case '-o':
-      case '--output':
-        nextKey = 'outputPath';
-        break;
-      case '-e':
-      case '--eval':
-        nextKey = '';
-        options.isForGlobal = false;
-        break;
-      case '-s':
-      case '--strict':
-        options.isForGlobal = true;
-        break;
-      default:
-        if (nextKey) {
-          options[nextKey] = val;
-          nextKey = undefined;
-        } else {
-          options.inputPath = val;
-        }
+  OPTIONS:
+    -o | --output    Snapshot output file path -- default is "snapshot.out"
+    -s | --strict    Source should be interpreted in strict mode -- default is false
+    -e | --eval      Code should be executed as eval() -- default is execute as global`);
+      process.exit(-1);
     }
-  });
 
-  var fs = require('fs');
-  if (!options.inputPath || !options.outputPath) {
-    printUsageAndExit();
+    // inputPath, isForGlobal, isStrict, outputPath
+    var options = {
+      inputPath: undefined,
+      outputPath: 'snapshot.out',
+      isForGlobal: true,
+      isStrict: false,
+    };
+
+    var nextKey;
+    process.argv.slice(2).forEach((val, index) => {
+      switch (val) {
+        case '-o':
+        case '--output':
+          nextKey = 'outputPath';
+          break;
+        case '-e':
+        case '--eval':
+          nextKey = '';
+          options.isForGlobal = false;
+          break;
+        case '-s':
+        case '--strict':
+          options.isForGlobal = true;
+          break;
+        default:
+          if (nextKey) {
+            options[nextKey] = val;
+            nextKey = undefined;
+          } else {
+            options.inputPath = val;
+          }
+      }
+    });
+
+    var fs = require('fs');
+    if (!options.inputPath || !options.outputPath) {
+      printUsageAndExit();
+    }
+
+    if (!fs.existsSync(options.inputPath)) {
+      console.error(`Input file does not exist: ${options.inputPath}`);
+      process.exit(-1);
+    }
+
+    compileFile(
+      options.inputPath, options.isForGlobal,
+      options.isStrict, options.outputPath);
+    process.exit(0);
   }
-
-  if (!fs.existsSync(options.inputPath)) {
-    console.error(`Input file does not exist: ${options.inputPath}`);
-    process.exit(-1);
-  }
-
-  var compiler = new Module();
-  compiler.compileFile(
-    options.inputPath, options.isForGlobal,
-    options.isStrict, options.outputPath);
-  process.exit(0);
-}
-
-})(); // IIFE that is started in pre.js
+};
--- emscripten-snapshot-compiler/pre.js
+++ emscripten-snapshot-compiler/pre.js
@@ -12,97 +12,82 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-(function() { // IIFE ends in post.js
-var Module;
-var ENVIRONMENT_IS_NODE;
+function isArrayOrNodeBuffer(b) {
+  if (typeof b !== 'object') {
+    return false;
+  }
+  if (b instanceof Array) {
+    return true;
+  }
+  // Check in case this code is not running in Node.js
+  if (typeof Buffer !== 'function') {
+    return false;
+  }
+  return (b instanceof Buffer);
+}
 
-(function() {
-  var prototype = {};
+function writeToFile(outputPath, outputArray) {
+  if (!outputPath) {
+    return;
+  }
+  if (!ENVIRONMENT_IS_NODE) {
+    throw new Error(
+      'Writing to file is only available when running with Node.js');
+  }
+  var fs = require('fs');
+  fs.writeFileSync(outputPath, outputArray);
+}
 
-  function isArrayOrNodeBuffer(b) {
-    if (typeof b !== 'object') {
-      return false;
-    }
-    if (b instanceof Array) {
-      return true;
-    }
-    // Check in case this code is not running in Node.js
-    if (typeof Buffer !== 'function') {
-      return false;
-    }
-    return (b instanceof Buffer);
+function compileBuffer(inBuffer, inBufferSize, isForGlobal, isStrict, outputPath) {
+  isForGlobal = (isForGlobal === undefined) ? true : Boolean(isForGlobal);
+  isStrict = Boolean(isStrict);
+
+  var snapshotOutputBufferSize = 512 * 1024;
+  var snapshotOutputBuffer = Module._malloc(snapshotOutputBufferSize);
+  var inBufferType;
+  if (typeof inBuffer === 'number') {
+    inBufferType = 'number';
+  } else if (isArrayOrNodeBuffer(inBuffer)) {
+    inBufferType = 'array';
+  } else {
+    throw new Error('Unexpected inBuffer type: ' + inBuffer);
   }
 
-  function writeToFile(outputPath, outputArray) {
-    if (!outputPath) {
-      return;
-    }
-    if (!ENVIRONMENT_IS_NODE) {
-      throw new Error(
-        'Writing to file is only available when running with Node.js');
-    }
-    var fs = require('fs');
-    fs.writeFileSync(outputPath, outputArray);
+
+  var emscripten_snapshot_compiler_compile = Module.cwrap('emscripten_snapshot_compiler_compile', 'number',
+  														[inBufferType, 'number', 'number', 'number', 'number', 'number']);
+
+  var bytesWritten = emscripten_snapshot_compiler_compile(inBuffer, inBufferSize, isForGlobal, isStrict, snapshotOutputBuffer, snapshotOutputBufferSize);
+  if (0 === bytesWritten) {
+    throw new Error('No snapshot bytes written');
   }
+  var rv = Module.HEAPU8.subarray(
+    snapshotOutputBuffer, snapshotOutputBuffer + bytesWritten);
+  Module._free(snapshotOutputBuffer);
+  writeToFile(outputPath, rv);
+  return rv;
+}
 
-  function compileBuffer(inBuffer, inBufferSize, isForGlobal, isStrict, outputPath) {
-    isForGlobal = (isForGlobal === undefined) ? true : Boolean(isForGlobal);
-    isStrict = Boolean(isStrict);
-
-    var snapshotOutputBufferSize = 512 * 1024;
-    var snapshotOutputBuffer = Module._malloc(snapshotOutputBufferSize);
-    var inBufferType;
-    if (typeof inBuffer === 'number') {
-      inBufferType = 'number';
-    } else if (isArrayOrNodeBuffer(inBuffer)) {
-      inBufferType = 'array';
-    } else {
-      throw new Error('Unexpected inBuffer type: ' + inBuffer);
-    }
-    var bytesWritten = Module.ccall(
-      'emscripten_snapshot_compiler_compile', 'number',
-      [inBufferType, 'number', 'number', 'number', 'number', 'number'],
-      [inBuffer, inBufferSize, isForGlobal, isStrict,
-        snapshotOutputBuffer, snapshotOutputBufferSize]);
-    if (0 === bytesWritten) {
-      throw new Error('No snapshot bytes written');
-    }
-    var rv = Module.HEAPU8.subarray(
-      snapshotOutputBuffer, snapshotOutputBuffer + bytesWritten);
-    Module._free(snapshotOutputBuffer);
-    writeToFile(outputPath, rv);
-    return rv;
+function compileString(sourceString, isForGlobal, isStrict, outputPath) {
+  var sourceBufferSize = lengthBytesUTF8(sourceString) + 1;
+  var sourceUTF8Buffer = Module._malloc(sourceBufferSize);
+  var writtenUTF8Size = stringToUTF8(sourceString, sourceUTF8Buffer, sourceBufferSize);
+  if (sourceBufferSize < writtenUTF8Size) {
+    throw new Error('Error decoding sourceString');
   }
+  var rv = compileBuffer(
+    sourceUTF8Buffer, sourceBufferSize - 1, isForGlobal, isStrict, outputPath);
+  Module._free(sourceUTF8Buffer);
+  return rv;
+};
 
-  prototype.compileString = function(sourceString, isForGlobal, isStrict, outputPath) {
-    var sourceBufferSize = Module.lengthBytesUTF8(sourceString) + 1;
-    var sourceUTF8Buffer = Module._malloc(sourceBufferSize);
-    var writtenUTF8Size = Module.stringToUTF8(sourceString, sourceUTF8Buffer, sourceBufferSize);
-    if (sourceBufferSize < writtenUTF8Size) {
-      throw new Error('Error decoding sourceString');
-    }
-    var rv = compileBuffer(
-      sourceUTF8Buffer, sourceBufferSize - 1, isForGlobal, isStrict, outputPath);
-    Module._free(sourceUTF8Buffer);
-    return rv;
-  };
-
-  prototype.compileFile = function(inputPath, isForGlobal, isStrict, outputPath) {
-    if (!ENVIRONMENT_IS_NODE) {
-      throw new Error(
-        'compileFile() is only available when running with Node.js');
-    }
-    var fs = require('fs');
-    var sourceInputBuffer = fs.readFileSync(inputPath);
-    return compileBuffer(
-      sourceInputBuffer, sourceInputBuffer.length, isForGlobal, isStrict, outputPath);
-  };
-
-  prototype.inspect = prototype.toString = function() {
-    return '[JerryScriptSnapshotCompiler v' + prototype.snapshotVersion + ']';
-  };
-
-  var ctor = function() {};
-  ctor.prototype = prototype;
-  Module = ctor;
-})();
+function compileFile(inputPath, isForGlobal, isStrict, outputPath) {
+  if (!ENVIRONMENT_IS_NODE) {
+    throw new Error(
+      'compileFile() is only available when running with Node.js');
+  }
+  var fs = require('fs');
+  var sourceInputBuffer = fs.readFileSync(inputPath);
+  return compileBuffer(
+    sourceInputBuffer, sourceInputBuffer.length, isForGlobal, isStrict, outputPath);
+}
